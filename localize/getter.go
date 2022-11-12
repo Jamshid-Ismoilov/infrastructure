4638d046042f6995bf1d4680c53d1b0e6e375d84
Jamshid Rustamjon o'g'li <jamshid@jamshid-Aspire-A315-55G.(none)>
2022-11-12 13:03:14 +0500

    new logic

--- localize/getter.go
+++ localize/getter.go
@@ -2,35 +2,49 @@ package localize
 
 import "errors"
 
+// returns localizer for service according to name
 func NewLocalizer(service string) (server ServiceGetter) {
 	serviceMap, ok := WordSource[service]
 	if !ok {
 		return ServiceGetter{Error: errors.New("Error in service name. (Cannot find service)")}
 	}
 	return ServiceGetter{
-		Service: serviceMap,
+		service: serviceMap,
 		Error:   nil,
 	}
 }
 
-type WordGetter struct {
-	word  map[string]string
-	Error error
-}
+// getter for service
 type ServiceGetter struct {
-	Service map[string]map[string]string
+	service map[string]map[string]string
 	Error   error
 }
 
-func (s ServiceGetter) Localize(word string) WordGetter {
-	wordMap, ok := s.Service[word]
+// returns translation of word accourding to word name and language
+func (s ServiceGetter) Localize(word string, lang string) (localizedWord string) {
+	localizedWord, ok := s.service[word][lang]
 	if !ok {
-		return WordGetter{Error: errors.New("Error in word. (Cannot find word)")}
+		return ""
+	}
+	return localizedWord
+}
+
+// returns word type, which includes localization settings
+func (s *ServiceGetter) GetWord(word string) WordGetter {
+	wordMap, ok := s.service[word]
+	if !ok {
+		return WordGetter{Error: errors.New("Error in word name. (Cannot find word)")}
 	}
 	return WordGetter{
 		word:  wordMap,
 		Error: nil,
 	}
+}
+
+// getter for word
+type WordGetter struct {
+	word  map[string]string
+	Error error
 }
 
 func (l *WordGetter) Uzbek() string {
